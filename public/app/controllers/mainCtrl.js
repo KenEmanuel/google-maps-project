32833a864ae7b38a3d3579296b857b2d7b2831ac
Kenneth Emanuel <ken.emanuel@gmail.com>
2015-08-04 13:50:34 -0700

    Set up angular-google-maps.

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,6 +1,16 @@
-angular.module('mainCtrl', [])
-    .controller('mainController', function() {
-        var vm = this;
+angular.module('mainCtrl', ['uiGmapgoogle-maps'])
+    .config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyAAQS8Zu1XNUerotmsJQYd7ezdEPwTu648',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+})
+    .controller('mainController', function($scope, uiGmapGoogleMapApi) {
 
-        vm.message = 'test message';
+        $scope.message = 'test message';
+
+        uiGmapGoogleMapApi.then(function(maps) {
+            $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+        });
     });
